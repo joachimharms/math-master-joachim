df305639aca375bb49f540cc531abb04d4cb8670
joachimharms <joachimharms77@gmail.com>
2017-10-29 01:08:40 +0200

    Finish the first unit test with xUnit Framework - done

--- myTests/MathHelper_MaxShould.cs
+++ myTests/MathHelper_MaxShould.cs
@@ -6,20 +6,28 @@ namespace myTests
 {
     public class MaxHelper_MaxShould
     {
+        private readonly MathHelper _mathHelper;
+
+        public MaxHelper_MaxShould()
+        {
+            _mathHelper = new MathHelper();
+        }
         [Fact]
         public void Return0GivenValuesOf0()
         {
-            var _mathHelper = new MathHelper(); 
             var result = _mathHelper.Max(0, 0);
 
-            if(result == 0)
-            {
-                System.Console.WriteLine("Success");
-            }
-            else
-            {
-                System.Console.WriteLine("Failed");
-            }
+            // Wir ersetzen folgende Logik durch die Assertfunktion, die von xUnit bereitgestellt wird.
+            // if(result == 0)
+            // {
+            //     System.Console.WriteLine("Success");
+            // }
+            // else
+            // {
+            //     System.Console.WriteLine("Failed");
+            // }
+
+            Assert.Equal(0, result);
         }
     }
 }
