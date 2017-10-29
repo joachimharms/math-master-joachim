2145a0504d8ea0f67f77b11384e9e559e33646db
joachimharms <joachimharms77@gmail.com>
2017-10-29 06:43:55 +0100

    Implemented more Testsets with [Theory] Attibute and tests are failing - done

--- myTests/MathHelper_MaxShould.cs
+++ myTests/MathHelper_MaxShould.cs
@@ -12,12 +12,18 @@ namespace myTests
         {
             _mathHelper = new MathHelper();
         }
-        [Fact]
-        public void Return0GivenValuesOf0()
+        [Theory]
+        [InlineDataAttribute(89,89, 89)]
+        [InlineDataAttribute(-40789,-40789, -40789)]
+        [InlineDataAttribute(275,275, 276)]
+        [InlineDataAttribute(1,1, 1)]
+        [InlineDataAttribute(9999,9999, 9999)]
+        [InlineDataAttribute(0,0, 0)]
+        public void ReturnValueWhenValueOfOperandsAreTheSame(int value1, int value2, int expectedResult)
         {
-            var result = _mathHelper.Max(0, 0);
+            var result = _mathHelper.Max(value1, value2);
 
-            Assert.Equal(0, result);
+            Assert.Equal(expectedResult, result);
         }
     }
 }
